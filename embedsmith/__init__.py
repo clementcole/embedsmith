6a527cc52a8448851df619f00d3faac5ee3c00d4
clementcole <clementacole75@gmail.com>
2025-10-15 00:36:54 -0500

    a few changes in work flow __init__

--- embedsmith/__init__.py
+++ embedsmith/__init__.py
@@ -3,7 +3,7 @@ embedsmith - A cli tool for creating standardized embedded project layouts.
 Craft your embedded firmware with structure and best practices from day one.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Clement Cole"
 __email__ = "clementacole75@gmail.com"
 
